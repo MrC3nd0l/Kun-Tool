50194d9eced897c2deccf6f648129b75a74a4293
MrC3nd0l <49186597+MrC3nd0l@users.noreply.github.com>
2019-06-11 21:19:39 +0700

    Add files via upload

---
+++ Kun-Tool.sh
@@ -0,0 +1,45 @@
+#usr/bin/sh
+h="\033[32;1m"
+k="\033[33;1m"
+b="\033[34;1m"
+m="\033[31;1m"
+u="\033[37;1m"
+p="\033[39;1m"
+clear
+figlet -f future "Kun-Tool"
+echo $m"///////////////////////////////////////////////////"
+echo $h"Author    = MrC3nd0l / _/|Arya-Kun||"
+echo $b"Support   = Den$"
+echo $k"Version   = 1.0"
+echo $p"Thx to    = _/|Youe-Kun|| - Mr.Red - _/|Naruto-Kun|| - Den$"
+echo $u"From      = D45H7 & Smiley Face"
+echo $p"///////////////////////////////////////////////////"
+sleep 1
+echo "Yakin mau nginstall semua tool? (90MB+)"
+echo $"Lu kudu punya space 500MB di penyimpanan lu ya vangke.."
+echo "y/n"
+read -p "root@Arya-Kun# " kampank
+
+if [ $kampank = "Y" ] || [ $kampank = "y" ]
+then
+   echo "Tools ini dikumpulkan oleh Den$"
+   sleep 1
+   echo "Recode? Tanya ma orang nya sendiri ya... awoakoqkkaq"
+   sleep 1
+   echo $h"Installing...."
+   sleep 3
+   mv Avojdbtucnakdoeoqueufppsncivus RadenTools.zip
+   unzip RadenTools.zip
+   echo $k"Use: cd RadenTools1/2/3/4"
+   echo $p"See U Later :)"
+   echo $m"Semoga Tools ini bermanfaat ya budjank.. :')"
+   exit
+fi
+
+if [ $kampank = "N" ] || [ $kampank = "n" ]
+then
+   echo $h "See U Later"
+   figlet -f future "D45H7 ft. Smiley Face"
+   exit
+fi
+
---
+++ bahan.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#:v
+#ini bahan bahan nya
+
+pkg install python
+pkg install python2
+pkg install figlet
+pkg install wget
+pkg install zip
+termux-fix-shebang Kun-Tool.sh
+chmod +x *
+echo "Woke gayn :)"
+echo "Siap untuk menjalankan tool nya"
+done
+exit
